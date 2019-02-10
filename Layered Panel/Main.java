d2654fc96a3ea35a85e2b932cf92ae473c4259ad
luke.murphy3@ucdconnect.ie <murphyluke01@gmail.com>
2019-02-10 21:34:57 +0000

    joined Panels

---
+++ Layered Panel/Main.java
@@ -0,0 +1,13 @@
+public class Main  
+{
+
+	public static void main(String[] args) 
+	{
+		JFrame frame = new CreateBoard();
+		frame.setTitle("BackGammon");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
