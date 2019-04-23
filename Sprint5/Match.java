6ad5780d1fed818d80fb49fd79f9c206f9dee7b3
seanennis <seanennis98@gmail.com>
2019-04-23 23:24:34 +0100

    Profs Updated Code

--- Sprint5/Match.java
+++ Sprint5/Match.java
@@ -78,4 +78,7 @@ public class Match implements MatchAPI {
             crawfordDone = false;
         }
 
+        public boolean canDouble(int playerId) {
+            return canDouble(players.get(playerId));
+        }
     }
